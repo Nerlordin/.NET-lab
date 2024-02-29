e602c504f6528b34ec7bb4f3eb9bb03957367d1a
Nerlordin <faironmail@gmail.com>
2024-02-29 12:09:38 +0100

    Create a simple web application in MVC architecture

---
+++ Widoki/Widoki/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Widoki.Models;
+
+namespace Widoki.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly PhoneBookService _phoneBook;
+
+        public HomeController(ILogger<HomeController> logger, PhoneBookService phoneBook)
+        {
+            _logger = logger;
+            _phoneBook = phoneBook;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_phoneBook.GetContacts());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+               
+            if (id != null)
+            {
+                _phoneBook.Remove(id);
+            }
+            else
+            {
+                return NotFound();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _phoneBook.Add(contact);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       
+
+    }
+}
---
+++ Widoki/Widoki/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+namespace Widoki.Models
+{
+    public class Contact
+    {
+        [DisplayName("Identyfikator")]
+        public int Id { get; set; }
+        [DisplayName("Imie")]
+        public String Name { get; set; }
+        [DisplayName("Nazwisko")]
+        public String Surname { get; set; }
+        [DisplayName("Email")]
+        public String Email { get; set; }
+        [DisplayName("Miasto")]
+        public String City { get; set; }
+        [DisplayName("Numer Telefonu")]
+        public String PhoneNumber { get; set; }
+
+        internal int FirstOrDefault(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
