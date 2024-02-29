4d4e2fb59d0d58eb23d7932beb30b69d2d76e512
Nerlordin <faironmail@gmail.com>
2024-02-29 12:30:46 +0100

    Create Authentication

---
+++ WebAPI Authentication/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Lab9.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private UserManager<IdentityUser> _user;
+    private IConfiguration _configuration;
+
+    public UserController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+    {
+        _user = userManager;
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Token([FromBody] UserDto dto)
+    {
+        var user = await _user.FindByEmailAsync(dto.UserName);
+        if (user == null || !await _user.CheckPasswordAsync(user, dto.Password))
+        {
+            return BadRequest("Invalid username or password");
+        }
+
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+        };
+        foreach (var role in await _user.GetRolesAsync(user))
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Tokens:Issuer"],
+            audience: _configuration["Tokens:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(60),
+            signingCredentials: creds
+        );
+
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token),
+            expiration = token.ValidTo
+        });
+    }
+}
